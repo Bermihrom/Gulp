d7fb8422b9901cce8ab52828bbd7c1689ce13706
Bermihrom <funnyhak876@gmail.com>
2023-07-21 17:06:59 +0300

    BrowserSync is ready

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,28 @@ import gulp from "gulp";
 
 import { path } from "./gulp/config/path.js";
 
+import { plugins } from "./gulp/config/plugins.js";
+
+
 global.app = {
     path: path,
     gulp: gulp,
+    plugins: plugins,
 }
 
 
 import { copy } from "./gulp/task/copy.js";
 import { reset } from "./gulp/task/reset.js";
+import { html } from "./gulp/task/html.js";
+import { server } from "./gulp/task/server.js";
 
 
 function watcher(){
-    gulp.watch(path.watch.files, copy)
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
 }
 
-const dev = gulp.series(reset, copy, watcher);
+const mainTask = gulp.parallel(copy, html);
+const dev = gulp.series(reset, mainTask, gulp.parallel(watcher, server));
 
 gulp.task('default', dev);
